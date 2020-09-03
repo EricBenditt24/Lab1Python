e71b6c044f9e5e169b9ffc89d6f1c95c1ee65f73
EricBenditt <erb5623@psu.edu>
2020-09-03 17:24:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author: Eric Benditt erb5623@psu.edu
+# Collaborator: Khalid AlMahood kfa5270@psu.edu
+# Collaborator: Gabrielle Brunner-King gsb5225@psu.edu
+# Collaborator: Bakhtiar Reza bmr5782@psu.edu
+
+temp= input("Enter temperature: ") 
+unit= input("Enter unit in F/f or C/c: ") 
+temp= float(temp) 
+if (unit == "F" or unit == "f"):
+  print(f"{temp}° in Fahrenheit is equivalent to {(temp-32)*(5/9)}° Celsius.") 
+elif (unit == "C" or unit == "c"): 
+  print(f"{temp}° in Celsius is equivalent to {(temp*1.8)+32}° Fahrenheit.") 
+  print("Invalid unit("+ unit +").") 
