104314f608cb30444511d0b36e5bda5e748f98b5
EricBenditt <erb5623@psu.edu>
2020-09-03 18:06:49 +0000

    Added the line else:

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ if (unit == "F" or unit == "f"):
   print(f"{temp}° in Fahrenheit is equivalent to {(temp-32)*(5/9)}° Celsius.") 
 elif (unit == "C" or unit == "c"): 
   print(f"{temp}° in Celsius is equivalent to {(temp*1.8)+32}° Fahrenheit.") 
-  print("Invalid unit("+ unit +").") 
+else:
+  print("Invalid unit(" + unit +")")
